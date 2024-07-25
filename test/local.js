5460b2c4ae2c1974f7b6bc3c70f65401a9c2f149
bronifty <bronifty@gmail.com>
2024-07-25 10:11:48 -0400

    this

---
+++ test/local.js
@@ -0,0 +1,17 @@
+import { handler, app } from "../deploy/lambda.cjs";
+import { normalizeRequest } from "../utils/normalize-request.js";
+import apigRequest from "../events/apig.js";
+import cloudfrontRequest from "../events/cloudfront.js";
+
+async function main(event) {
+  const normalizedRequest = normalizeRequest(event);
+  const result = await handler(normalizedRequest);
+  console.log(result);
+}
+
+main(apigRequest);
+main(cloudfrontRequest);
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
